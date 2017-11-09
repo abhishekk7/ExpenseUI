e00e3cadb8e211956c223b94c20fed5dda52e035
abhishek <abhishek3433kumar@gmail.com>
2017-11-09 01:11:32 -0600

    changed api url

--- app/js/constants.js
+++ app/js/constants.js
@@ -1,6 +1,6 @@
 angular.module('Constants', []).service('Constants', [function () {
     return {
         //apiBaseUrl: 'http://192.168.0.28:9000/api/v1'
-        apiBaseUrl: 'https://afternoon-ridge-32210.herokuapp.com/api/v1'
+        apiBaseUrl: 'https://expense-api-v1.herokuapp.com/api/v1'
     }
 }]);
